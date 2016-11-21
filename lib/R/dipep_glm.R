6775ef2ab06b2579dd6dc8e6f8356f0151a08748
Neil Shephard <n.shephard@sheffield.ac.uk>
2016-11-21 09:10:02 +0000

    Solved the Non-Standard Evaluation of select()'ing multiple variables.

--- lib/R/dipep_glm.R
+++ lib/R/dipep_glm.R
@@ -12,51 +12,38 @@
 #'
 #' @param df Data frame to analyse (default is \code{dipep} and shouldn't need changing)
 #' @param predictor Predictor variable(s) to test.
-#' @param model Name of your model.
+#' @param model Name/label of your model.
 #'
 #'
 #' @export
 dipep_glm <- function(df              = .data,
-                      predictor       = c('age'),
+                      predictor       = 'age',
                       model           = 'age',
                       ...){
     results <- list()
     ## Build the formula
     .formula <- reformulate(response = 'pe',
                             termlabels = predictor)
-    print(.formula)
     ## Filter the data frame
-    to.select <- c('pe', predictor)
-    print(to.select)
     results$df <- dplyr::filter(df, group == 'Suspected PE') %>%
-        ## ToDo - FIX THIS!!!
-          ## dplyr::select_('pe', substitute(predictor)) %>%
-          dplyr::select_('pe', 'bmi', 'temperature', 'bp.diastolic', 'bp.systolic', 'o2.saturation') %>%
-          mutate(obs = rownames(.),
-                 use = complete.cases(.)) %>%
-          dplyr::filter(use == TRUE) %>%
-        dplyr::select(-use)
+                  dplyr::select_(.dots = c('pe', predictor)) %>%
+                  mutate(obs = rownames(.),
+                         use = complete.cases(.)) %>%
+                  dplyr::filter(use == TRUE) %>%
+                  dplyr::select(-use)
     results$df$obs <- rownames(results$df)
     ## Test the model
-    names(results$df) %>% print()
     results$fitted <- glm(.formula,
                           data   = results$df,
                           family = 'binomial')
-    print(results$fitted)
     results$tidied    <- broom::tidy(results$fitted)
-    print('Debug 1')
     results$augmented <- broom::augment(results$fitted)
-    print('Debug 2')
     results$glance    <- broom::glance(results$fitted)
-    print('Debug 3')
     ## Add the predictor to each to make it easier combining
     results$tidied$model    <- model
-    print('Debug 4')
     results$augmented$model <- model
-    print('Debug 5')
     results$glance$model    <- model
     ## Get predicted probabilities
-    print('Debug 6')
     results$predicted <- results$fitted %>% predict() %>% as.data.frame()
     ## names(results$predicted) <- paste0(predictor, '.predicted')
     names(results$predicted) <- c('pred')
