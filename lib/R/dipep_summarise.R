aa4ba4f63d8397dc4796b423862294ac58f34d83
Neil Shephard <n.shephard@sheffield.ac.uk>
2017-02-16 10:27:24 +0000

    Working function, handles arbitrary grouping (so works with multiple classifications)

--- lib/R/dipep_summarise.R
+++ lib/R/dipep_summarise.R
@@ -5,41 +5,86 @@
 #' @details
 #' Many quantitative assessments are made, it is useful to know, the mean, standard deviation
 #' (SD), median and inter-quartile range (IQR), minimum and maximum.  This funciton calculates
-#' these for a given
+#' these for a given grouing
 #'
 #'
 #' @param df Data frame holding data
-#' @param by Variable to group data by.
+#' @param by Variable to group data by, options are \code{group} | \code{primary.class} | \code{secondary.class}.
 #' @param to.sum Variable to be summarised.
-#' @param na.rm How to handle NAs (default is )
-#' @param mean Logical indicator of whether to calculate the Mean
 #'
 #' @export
-dipep_summarise <- function(df              = .data,
+dipep_summarise <- function(df              = dipep,
                             grouping        = 'group',
-                            to.sum          = c('respiratory.rate', 'heart.rate'),
-                            na.rm           = 'TRUE',
-                            ## mean            = TRUE,
-                            ## sd              = TRUE,
-                            ## p25             = TRUE,
-                            ## median          = TRUE,
-                            ## p75             = TRUE,
-                            ## min             = TRUE,
-                            ## max             = TRUE,
-                            ## missing         = TRUE,
+                            group.as.col    = FALSE,
                             ...){
     results <- list()
-    ## Subset the data
-    df <- dplyr::select_(df, .dots = c(grouping, to.sum))
-    head(df) %>% print()
-    mean(df$respiratory.rate, na.rm = TRUE) %>% print()
-    ## Overall
-    overall <- summarise_each_(df,
-                               funs(mean(na.rm = TRUE),
-                                    sd(na.rm = TRUE)),
-                               .dots = to.sum)
-                               ## vars(~to.sum))
-    overall %>% print()
-    ## By Group
+    ## Get the levels for the grouping
+    observed.levels <- table(df[grouping], useNA = 'ifany') %>% names()
+    observed.levels[is.na(observed.levels)] <- 'Exclude'
+    ## Overall summary
+    overall <- dplyr::select_(df, .dots = lazyeval::lazy_dots(...)) %>%
+               summarise_each(funs(N      = sum(!is.na(.)),
+                                   mean   = mean(., na.rm = TRUE),
+                                   sd     = sd(., na.rm = TRUE),
+                                   median = quantile(., probs = 0.5, na.rm = TRUE),
+                                   p25    = quantile(., probs = 0.25, na.rm = TRUE),
+                                   p75    = quantile(., probs = 0.75, na.rm = TRUE),
+                                   min    = min(., na.rm = TRUE),
+                                   max    = max(., na.rm = TRUE)))
+    overall[grouping] <- 'All'
+    ## Summarise by grouping
+    results <- group_by_(df, grouping) %>%
+               dplyr::select_(.dots = lazyeval::lazy_dots(...)) %>%
+               summarise_each(funs(N      = sum(!is.na(.)),
+                                   mean   = mean(., na.rm = TRUE),
+                                   sd     = sd(., na.rm = TRUE),
+                                   median = quantile(., probs = 0.5, na.rm = TRUE),
+                                   p25    = quantile(., probs = 0.25, na.rm = TRUE),
+                                   p75    = quantile(., probs = 0.75, na.rm = TRUE),
+                                   min    = min(., na.rm = TRUE),
+                                   max    = max(., na.rm = TRUE)))
+    ## Combine Overall and by group
+    results <- rbind(overall, results)
+    ## Conditionally reshape the data
+    if(group.as.col == TRUE){
+        results <- results %>%
+                   t() %>%
+                   data.frame() %>%
+                   mutate(Statistic = row.names(.),
+                          Measurement = gsub('_.*', '', Statistic),
+                          Measurement = gsub('age', 'Age', Measurement),
+                          Measurement = gsub('bmi', 'BMI', Measurement),
+                          Measurement = gsub('heart.rate', 'Heart Rate', Measurement),
+                          Measurement = gsub('bp.systolic', 'Systolic BP', Measurement),
+                          Measurement = gsub('bp.diastolic', 'Diastolic BP', Measurement),
+                          Measurement = gsub('height', 'Height', Measurement),
+                          Measurement = gsub('weight', 'Weight', Measurement),
+                          Measurement = gsub('respiratory.rate', 'Respiratory Rate', Measurement),
+                          Measurement = gsub('o2.saturation', 'Oxygen Saturation', Measurement),
+                          Measurement = gsub('gestation', 'Gestation', Measurement),
+                          Statistic = gsub('.*_', '', Statistic),
+                          Statistic = gsub('mean', 'Mean', Statistic),
+                          Statistic = gsub('sd', 'SD', Statistic),
+                          Statistic = gsub('median', 'Median', Statistic),
+                          Statistic = gsub('p25', 'Lower Quartile', Statistic),
+                          Statistic = gsub('p75', 'Upper Quartile', Statistic),
+                          Statistic = gsub('min', 'Min', Statistic),
+                          Statistic = gsub('max', 'Max', Statistic)) %>%
+            dplyr::filter(Statistic != grouping)
+        ## Rename and order
+        names(results) <- c('All', observed.levels, 'Statistic', 'Measurement')
+        results <- mutate(results,
+                          order1 = 1,
+                          order1 = ifelse(Statistic == 'N'              , 1, order1),
+                          order1 = ifelse(Statistic == 'Mean'           , 2, order1),
+                          order1 = ifelse(Statistic == 'SD'             , 3, order1),
+                          order1 = ifelse(Statistic == 'Lower Quartile' , 4, order1),
+                          order1 = ifelse(Statistic == 'Median'         , 5, order1),
+                          order1 = ifelse(Statistic == 'Upper Quartile' , 6, order1),
+                          order1 = ifelse(Statistic == 'Min'            , 7, order1),
+                          order1 = ifelse(Statistic == 'Max'            , 8, order1)) %>%
+                   arrange(Measurement, order1) %>%
+                   dplyr::select(-order1)
+    }
     return(results)
 }
