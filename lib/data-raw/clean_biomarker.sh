60add60d508922ef9d0eb513494d7bdc936b5f79
Neil Shephard <n.shephard@sheffield.ac.uk>
2017-02-28 15:09:13 +0000

    Added two additional clauses for correcting screeening IDs

---
+++ lib/data-raw/clean_biomarker.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+## Clean the biomarker file of unnecessary headers and then
+## clean non-numeric values and correct corrupted identifiers
+grep -v 'citrated plasma samples' biomarker_20170216.csv | \
+    grep -v 'ETP Measured values' | \
+    grep -v 'mg/l' | \
+    grep -v 'Reference range' |
+    sed -e 's/no plasma//g' \
+	-e 's/ce//g' \
+	-e 's/s/S/g' \
+	-e 's/SO/S0/g' \
+	-e 's/nc//g' \
+	-e 's/NC//g' \
+	-e 's/<//g' \
+	-e 's/S\//S/g' \
+	-e 's/D\//D/g' \
+	-e 's/    (HIV +)//g' \
+	-e 's/s\//S/g' > biomarker_clean.csv
